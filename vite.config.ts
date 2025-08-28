cb63a21d9e6dc9a65106ead99f8252902d3ee5b6
Mehmoodch <mehmoodasgharch@gmail.com>
2025-08-28 16:38:59 +0400

    Switch to HashRouter for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
-  base: '/desert-splash-design/',
+  base: '/desert-splash-design/', // IMPORTANT!
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
   },
-});
+})
